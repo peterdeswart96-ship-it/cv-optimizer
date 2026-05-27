446446296b1e93fb7e790daf794550fa24d5391f
peterdeswart96-ship-it <peterdeswart96@gmail.com>
2026-05-27 12:21:09 +0200

    feat: register new backend functions in index

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
 require('./src/functions/analyze');
+require('./src/functions/extract');
+require('./src/functions/analyze-cv');
+require('./src/functions/analyze-section');
 require('./src/functions/upload');
 require('./src/functions/export');
